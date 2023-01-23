f21e96016913fe78a206c22e293410c82b9a6ef4
YaOn999 <lumous999@gmail.com>
2023-01-24 00:35:47 +0300

    added array input code

--- FP_001/Program.cs
+++ FP_001/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] NewMassive(int size)
+{
+    string[] stringArray = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i} элемент массива: ");
+        stringArray[i] = Console.ReadLine();
+    }
+    return stringArray;
+}
+
+Console.Write("Веедите размер массива: ");
+int size = int.Parse(Console.ReadLine());
+string[] stringArray = NewMassive(size);
+Console.WriteLine($"введенный массив [{String.Join(", ", stringArray)}]");
+
+
