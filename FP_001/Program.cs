b289b32df276b55ff2fad0a7cf8e268b4e1b9fa9
YaOn999 <lumous999@gmail.com>
2023-01-24 23:05:06 +0300

    add finish  code

--- FP_001/Program.cs
+++ FP_001/Program.cs
@@ -1,4 +1,29 @@
-﻿string[] NewMassive(int size)
+﻿string[] NewArray(string[] stringArray, int sizeNewArray)
+{
+    string[] newArray = new string[sizeNewArray];
+    int temp = 0;
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (stringArray[i].Length <= 3)
+        {
+            newArray[temp] = stringArray[i];
+            temp++;
+        }
+    }
+    return newArray;
+}
+
+int NewArraySize(string[] stringArray)
+{
+    int size = 0;
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (stringArray[i].Length <= 3) size++;
+    }
+    return size;
+}
+
+string[] NewMassive(int size)
 {
     string[] stringArray = new string[size];
     for (int i = 0; i < size; i++)
@@ -6,12 +31,17 @@
         Console.Write($"Введите {i} элемент массива: ");
         stringArray[i] = Console.ReadLine();
     }
+    Console.WriteLine($"введенный массив [{String.Join(", ", stringArray)}]");
     return stringArray;
 }
 
 Console.Write("Веедите размер массива: ");
 int size = int.Parse(Console.ReadLine());
+
 string[] stringArray = NewMassive(size);
-Console.WriteLine($"введенный массив [{String.Join(", ", stringArray)}]");
+int sizeNewArray = NewArraySize(stringArray);
+
+string[] newArray = NewArray(stringArray, sizeNewArray);
+Console.WriteLine($"новый массив [{String.Join(", ", newArray)}]");
 
 
